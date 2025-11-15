51d4cd5d74bbc63300931543edc833dad3e2feae
benhfink <benhfink@gmail.com>
2025-11-15 11:05:07 -0500

    pathing

--- MeepMeep/src/main/java/com/example/meepmeep/Meeps.java
+++ MeepMeep/src/main/java/com/example/meepmeep/Meeps.java
@@ -1,6 +1,7 @@
 package com.example.meepmeep;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -14,17 +15,7 @@ public class Meeps {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .build());
-
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, 36, 0)).turnTo(Math.toRadians(315)).waitSeconds(5).splineTo(new Vector2d(0, 15), Math.toRadians(90)).splineTo(new Vector2d(-12, 56), Math.toRadians(90)).splineTo(new Vector2d(-36, 36), Math.toRadians(135)).waitSeconds(2).splineTo(new Vector2d(0, 15), Math.toRadians(90)).splineTo(new Vector2d(16, 56), Math.toRadians(90)).splineTo(new Vector2d(-36, 36), Math.toRadians(135)).waitSeconds(2).splineTo(new Vector2d(0, 15), Math.toRadians(90)).splineTo(new Vector2d(40, 56), Math.toRadians(90)).splineTo(new Vector2d(-36, 36), Math.toRadians(135)).waitSeconds(2).build());
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
