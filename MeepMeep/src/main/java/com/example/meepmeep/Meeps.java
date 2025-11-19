f65f9c3daa60f5a976df91173fb3bc2faf947081
benhfink <benhfink@gmail.com>
2025-11-18 19:41:35 -0500

    pathing revision, 17s

--- MeepMeep/src/main/java/com/example/meepmeep/Meeps.java
+++ MeepMeep/src/main/java/com/example/meepmeep/Meeps.java
@@ -15,8 +15,8 @@ public class Meeps {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, 36, 0)).turnTo(Math.toRadians(315)).waitSeconds(5).splineTo(new Vector2d(0, 15), Math.toRadians(90)).splineTo(new Vector2d(-12, 56), Math.toRadians(90)).splineTo(new Vector2d(-36, 36), Math.toRadians(135)).waitSeconds(2).splineTo(new Vector2d(0, 15), Math.toRadians(90)).splineTo(new Vector2d(16, 56), Math.toRadians(90)).splineTo(new Vector2d(-36, 36), Math.toRadians(135)).waitSeconds(2).splineTo(new Vector2d(0, 15), Math.toRadians(90)).splineTo(new Vector2d(40, 56), Math.toRadians(90)).splineTo(new Vector2d(-36, 36), Math.toRadians(135)).waitSeconds(2).build());
-        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-0,0, Math.toRadians(270))).splineToLinearHeading(new Pose2d(-12, -20, Math.toRadians(270)), Math.toRadians(90)).splineToLinearHeading(new Pose2d(-12, -58, Math.toRadians(270)), Math.toRadians(90)).splineToLinearHeading(new Pose2d(0, 0, Math.toRadians(135)), Math.toRadians(90)).splineToLinearHeading(new Pose2d(12, -20, Math.toRadians(270)), Math.toRadians(90)).splineToLinearHeading(new Pose2d(12, -58, Math.toRadians(270)), Math.toRadians(90)).splineToLinearHeading(new Pose2d(0, 0, Math.toRadians(135)), Math.toRadians(90)).splineToLinearHeading(new Pose2d(36, -20, Math.toRadians(270)), Math.toRadians(90)).splineToLinearHeading(new Pose2d(36, -58, Math.toRadians(270)), Math.toRadians(90)).splineToLinearHeading(new Pose2d(0, 0, Math.toRadians(135)), Math.toRadians(90)).build());
+                meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
